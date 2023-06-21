8d0c4a11a86b430ff97ae5cbcaf3ca3038b8a311
Jamessfks <91133760+Jamessfks@users.noreply.github.com>
2023-06-21 11:26:39 +0800

    C12_Loops.ino

--- Programming_Exercise/C12_Loops/C12_Loops.ino
+++ Programming_Exercise/C12_Loops/C12_Loops.ino
@@ -1,13 +1,11 @@
 #include <Arduino.h>
 
 bool isPrime(int number) {
-  if (number < 2) {
+  if (number <= 1)
     return false;
-  }
-  for (int i = 2; i <= sqrt(number); i++) {
-    if (number % i == 0) {
+  for (int i = 2; i * i <= number; i++) {
+    if (number % i == 0)
       return false;
-    }
   }
   return true;
 }
@@ -17,24 +15,29 @@ void setup() {
 }
 
 void loop() {
+  Serial.println("Enter 'break' to stop the loop or press any key to continue...");
+  
+  while (!Serial.available()); // Wait for user input
+  String input = Serial.readStringUntil('\n');
+  
+  if (input == "break") {
+    Serial.println("Loop stopped.");
+    while (1); // Infinite loop to halt the program
+  }
+
+  Serial.println("Generating the first 30 prime numbers:");
+  
   int count = 0;
   int number = 2;
-  int primeNumbers[30];
-
+  
   while (count < 30) {
     if (isPrime(number)) {
-      primeNumbers[count] = number;
+      Serial.print(number);
+      Serial.print(" ");
       count++;
     }
     number++;
   }
-
-  for (int i = 0; i < 30; i++) {
-    Serial.println(primeNumbers[i]);
-    delay(1000);
-  }
-
-  while (1) {
-    // Infinite loop to keep the prime numbers displayed
-  }
+  
+  Serial.println();
 }
